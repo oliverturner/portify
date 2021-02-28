42d5c340b33421eca8160e309a268a1682e58877
Oliver Turner <oliver.turner@codedsignal.co.uk>
2021-02-28 23:28:50 +0000

    Initial commit

---
+++ app.arc
@@ -0,0 +1,12 @@
+@app
+portify
+
+@http
+get /api/top
+
+# @aws
+# profile default
+# region us-west-1
+
+@sandbox-startup
+echo "hello"
---
+++ test/http/api/top.spec.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const test = require("tape");
+const sandbox = require("@architect/sandbox");
+const nock = require("nock");
+
+const { ROOT_URL, getTop } = require("../../../src/http/api/top");
+const apiUrl = `${ROOT_URL}/v1`;
+
+const fixturePath = __dirname + "../../../fixtures";
+const topTracksResult = require(`${fixturePath}/top-tracks-result.json`);
+const headers = {
+  "Content-Type": "application/json",
+};
+
+test("Set up env", (t) => {
+  t.plan(1);
+  t.ok(sandbox, "sandbox loaded");
+});
+
+test("Mock request", async (t) => {
+  t.plan(1);
+
+  const tracksQuery = {
+    limit: 1,
+    time_range: "medium_term",
+  };
+
+  nock(apiUrl)
+    .get(`/me/top/tracks`)
+    .query(tracksQuery)
+    .replyWithFile(200, path.resolve(fixturePath, "top-tracks.json"), headers);
+
+  nock(apiUrl)
+    .get(`/audio-features`)
+    .query(true)
+    .replyWithFile(
+      200,
+      path.resolve(fixturePath, "top-tracks-audio.json"),
+      headers
+    );
+
+  const req = { query: tracksQuery };
+  const res = await getTop(req);
+
+  t.deepEqual(res, topTracksResult, "top tracks results match");
+});
