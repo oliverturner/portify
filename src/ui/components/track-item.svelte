c70a29dfd565f9a86454bb6bdf1dc394c1d7b7ba
Oliver Turner <oliver.turner@codedsignal.co.uk>
2021-03-22 21:47:54 +0000

    Stub out story

---
+++ src/ui/components/track-item.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let title;
+
+	/**
+	 * Is this the principal call to action on the page?
+	 */
+	export let primary = false;
+
+	/**
+	 * What background color to use
+	 */
+	export let backgroundColor;
+	/**
+	 * How large should the button be?
+	 */
+	export let size = "medium";
+	/**
+	 * Button contents
+	 */
+	export let label = "";
+
+	let mode = primary
+		? "storybook-button--primary"
+		: "storybook-button--secondary";
+
+	let style = backgroundColor ? `background-color: ${backgroundColor}` : "";
+</script>
+
+<h1>Track Item</h1>
